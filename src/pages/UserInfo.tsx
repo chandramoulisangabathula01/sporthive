2bcd6cd2260a15a826f9030174bf66e74b3b41bd
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-05-28 12:55:23 +0530

    error while changing the password

--- src/pages/UserInfo.tsx
+++ src/pages/UserInfo.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
+import ChangePassword from './password/PasswordChange';
+
 
 const UserInfo: React.FC = () => {
-  const userDataString = localStorage.getItem('userData');
-  const userData = userDataString ? JSON.parse(userDataString) : null;
+    const [showChangePasswordModal, setShowChangePasswordModal] = useState<boolean>(false);
 
-  return (
-    <>
-      <Navbar />
-      <div className='w-screen h-screen bg-gray-900 flex items-start justify-center'>
-        <div className="max-w-md mt-16 p-6 bg-gray-500 rounded-lg">
-          <h2 className="text-2xl font-bold mb-4">
-            User Details
-            <hr className='my-2' />
-          </h2>
-          <p className="mb-2"><strong>Name:</strong> {userData.name}</p>
-          <p className="mb-2"><strong>Email:</strong> {userData.email}</p>
-        </div>
-      </div>
-    </>
-  );
+    const userDataString = localStorage.getItem('userData');
+    const userData = userDataString ? JSON.parse(userDataString) : null;
+
+    const handleCloseChangePasswordModal = () => {
+        setShowChangePasswordModal(false);
+    };
+
+    return (
+        <>
+            <Navbar />
+            <div className='w-screen h-screen bg-gray-900 flex items-start justify-center'>
+                <div className="max-w-md mt-16 p-6 bg-gray-800 rounded-lg">
+                    <h2 className="text-2xl font-bold mb-4 text-white">
+                        User Details
+                        <hr className='my-2' />
+                    </h2>
+                    <p className="mb-2 text-white"><strong>Name:</strong> {userData.name}</p>
+                    <p className="mb-2 text-white"><strong>Email:</strong> {userData.email}</p>
+                    <button onClick={() => setShowChangePasswordModal(true)}
+                        className="bg-gray-700 hover:bg-gray-500 mt-2 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-gray-500"
+                    >Change Password</button>
+                </div>
+
+            </div>
+
+            {showChangePasswordModal && (
+                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60">
+                    <ChangePassword closeModal={handleCloseChangePasswordModal} />
+                </div>
+            )}
+        </>
+    );
 };
 
 export default UserInfo;
---
+++ src/pages/password/PasswordChange.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { API_ENDPOINT } from '../../config/constants';
+
+
+interface ChangePasswordProps {
+    closeModal: () => void;
+}
+
+const ChangePassword: React.FC<ChangePasswordProps> = ({ closeModal }) => {
+    const [currentPassword, setCurrentPassword] = useState<string>('');
+    const [newPassword, setNewPassword] = useState<string>('');
+    const [showSuccessMessage, setShowSuccessMessage] = useState<boolean>(false);
+    const authToken = localStorage.getItem('authToken');
+
+    const handleChangePassword = async () => {
+        try {
+            const response = await fetch(`${API_ENDPOINT}/user/password`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${authToken}`,
+                },
+                body: JSON.stringify({
+                    current_password: currentPassword,
+                    new_password: newPassword,
+                }),
+            });
+            if (response.ok) {
+                // Password successfully updated, show success message
+                console.log('Password updated successfully');
+                setShowSuccessMessage(true);
+                setTimeout(() => {
+                    setShowSuccessMessage(false); // Hide success message after 3 seconds
+                    closeModal(); // Close the modal after successful password change
+                }, 3000);
+            } else {
+                // Handle error cases
+                console.error('Error updating password');
+            }
+        } catch (error) {
+            console.error('Error updating password:', error);
+        }
+    };
+
+    return (
+        <div className="bg-gray-600 p-6 w-1/3 rounded-md relative">
+            {showSuccessMessage && (
+                <div className="bg-green-200 text-green-700 px-4 py-2 rounded-md">
+                    Password Changed Successfully
+                </div>
+            )}
+            <h2 className="text-2xl font-bold mb-4">Change Password</h2>
+            <div className="mb-4">
+                <label htmlFor="currentPassword" className="block mb-2  font-semibold">Current Password</label>
+                <input type="password" id="currentPassword" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} className="border border-gray-500 px-3 py-2 bg-gray-300 rounded-lg w-full" />
+            </div>
+            <div className="mb-4">
+                <label htmlFor="newPassword" className="block mb-2 font-semibold">New Password</label>
+                <input type="password" id="newPassword" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} className="border border-gray-500 px-3 py-2 bg-gray-300 rounded-lg w-full" />
+            </div>
+            <div className="flex justify-end gap-2">
+                <button onClick={closeModal} className="bg-red-700 hover:bg-red-800 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-gray-500 border border-red-800  "
+                >Cancel</button>
+                <button onClick={handleChangePassword} className="bg-green-700 hover:bg-green-800 text-white px-4 py-2 rounded-full transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-gray-500 border-green-800"
+                >Save</button>
+            </div>
+        </div>
+    );
+};
+
+export default ChangePassword;
