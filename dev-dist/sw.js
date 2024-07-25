c70619e385c1c633846b0f9a6dea20c38cba5ee0
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-07-25 21:44:50 +0530

    work check 01

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l45jdl8favg"
+    "revision": "0.0q82jk4go4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
