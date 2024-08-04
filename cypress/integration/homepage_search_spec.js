8638caed5eedd4adda76ee09e69e62eace0006ee
chandramoulisangabathula01 <127196627+chandramoulisangabathula01@users.noreply.github.com>
2024-08-04 14:15:50 +0530

    test added & github action

---
+++ cypress/integration/homepage_search_spec.js
@@ -0,0 +1,9 @@
+// describe('Testing Wikipedia', () => {
+//     it('I can search for content', () => {
+//       cy.visit('https://www.wikipedia.org');
+//       cy.get("input[type='search']").type('Leo Panthera');
+//       cy.get("button[type='submit']").click();
+//       cy.contains('Search results');
+//       cy.contains('Panthera leo leo');
+//     });
+//   });
